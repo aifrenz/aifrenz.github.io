3b3a9a42377569d3ebdbce1888ffb655c7c90ec1
ai-friends <47202763+ai-friends@users.noreply.github.com>
2019-10-14 09:10:47 +0900

    Create 2019_10_02_membershipday.mk

---
+++ _posts/2019_10_02_membershipday.mk
@@ -0,0 +1,31 @@
+---
+layout: post
+title:  AI 프렌즈 멤버쉽 DAY
+when: 2019. 10. 2. (수) 오후 7시-9시
+where: 대전 TBC 1층 콜라보홀
+who: 유용균
+what: 바이올린, 최적설계 그리고 인공지능
+date:   2019-10-02 19:00:00
+categories: ai-friends update
+tags: ai-friends update
+image: /images/20191002.jpg
+---
+***  
+바이올린, 최적설계 그리고 인공지능 ( 유용균 한국원자력연구원 )
+[발표자료]({{ "/present_file/191002_AI프렌즈_resize.pdf" | absolute_url }})  
+
+
+
+- 모임 : 10월 AI 프렌즈 멤버쉽 DAY  
+- 일시 : 2019년 10월 2일 수요일 19:00 ~ 21:00  
+- 장소 : 대덕테크비즈센터(TBC) 1층 콜라보홀  
+             (주소 : 대전 유성구 대덕대로 593, http://naver.me/GEfICdhv)  
+- 대상 : AI에 관심있는 누구나  
+             (단, 이번 시즌은 최소 MNIST정도는 돌려보신 분들을 대상으로 합니다.)  
+
+
+
+- 후원 : 연구개발특구진흥재단  
+- 문의 : 한국원자력연구원 유용균 박사(yoyogo@gmail.com)  
+             한국전자통신연구원 김귀훈 박사(kiwi7580@gmail.com)  
+- 사이트 : https://aifrenz.github.io/ 
